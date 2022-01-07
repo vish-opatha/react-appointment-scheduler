765fbec353b8826c09ecfcd84ba6ab63ac4f73d5
Vish O <v.opatha@gmail.com>
2022-01-07 14:56:29 +1030

    Add the Search Component to the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import {GoAlert} from "react-icons/go";
+import {GoAlert} from "react-icons/go"
+import Search from "./components/Search"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Appointment Scheduler</h1>
-      <GoAlert/>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl content-center">
+        <GoAlert className="inline-block text-blue-800 align-top"/>Appointment Scheduler
+      </h1>
+      <Search/>
     </div>
   );
 }
