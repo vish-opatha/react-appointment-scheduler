f61517fd1f898a245fec9baf8c98c2ce215474b1
Vish O <v.opatha@gmail.com>
2022-01-15 15:15:11 +1030

    Update Search component to get the search parameter on change of input

--- src/components/Search.js
+++ src/components/Search.js
@@ -34,7 +34,7 @@ const Dropdown = ({toggle}) =>{
 }
 
 // ******** Search Component ********
-const Search = () => {
+const Search = ({query, onQueryChange}) => {
   let [toggleSort, setToggleSort] = useState(false)
 
   return (
@@ -44,7 +44,8 @@ const Search = () => {
           <BiSearch />
           <label htmlFor="query" className="sr-only" />
         </div>
-        <input type="text" name="query" id="query" value=""
+        <input type="text" name="query" id="query" value={query}
+          onChange={(event)=>{onQueryChange(event.target.value)}}
           className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
         <div className="absolute inset-y-0 right-0 flex items-center">
           <div>
