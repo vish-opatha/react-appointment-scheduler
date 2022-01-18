9eb2cd739eb46b6465cd2dd925403203f71d72e8
Vish O <v.opatha@gmail.com>
2022-01-18 11:22:09 +1030

    Include basic sorting and ordering to the search results

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import AppointmentInfo from "./components/AppointmentInfo"
 function App() {
   let [appointmentData, setAppointmentData] = useState([]);
   let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("desc");
 
   const filteredAppt = appointmentData.filter(
     item=>{
@@ -16,7 +18,13 @@ function App() {
         item.aptNotes.toLowerCase().includes(query.toLowerCase()) 
       )
     }
-  )
+  ).sort((a,b) => {
+    let order = (orderBy === "asc") ? 1 : -1
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+      ? -1*order : 1*order
+    )
+  })
 
   const fetchAppointmentData = useCallback(() => {
     fetch('./data.json')
